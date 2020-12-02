8c7a2ad27f6654190417743f540280c47574533a
DouglasStuhler <DouglasStuhler@github.com>
2020-12-02 17:07:54 -0300

    inicio css index

---
+++ index.php
@@ -0,0 +1,31 @@
+<html>
+    <head>
+        <title>Projeto Magic</title>
+        <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+
+        <link rel="preconnect" href="https://fonts.gstatic.com">
+        <link href="https://fonts.googleapis.com/css2?family=Open+Sans:ital,wght@0,300;0,400;0,600;0,700;0,800;1,300;1,400;1,600;1,700;1,800&display=swap" rel="stylesheet">
+
+        <link rel="stylesheet" type="text/css" href="index.css">
+    </head>
+    <body>
+        <header class="menu">
+            <a href="#"><img src="logo.png" class="logo"></a>
+            <ul>
+                <li class="item"><a href="banner">Home</a></li>
+                <li class="item"><a href="">O que é RPG?</a></li>
+                <li class="item"><a href="">Objetivo</a></li>
+                <li class="item"><a href="">Criadores</a></li>
+                <li class="item"><a href="">Login</a></li>
+            </ul>
+        </header>
+        <div id="banner" class="banner">
+            <img src="logo.png">
+            <p class="txt">Mussum Ipsum, cacilds vidis litro abertis. Suco de cevadiss deixa as pessoas mais interessantis. Casamentiss faiz malandris se pirulitá. Nec orci ornare consequat. Praesent lacinia ultrices consectetur. Sed non ipsum felis. Paisis, filhis, espiritis santis.</p>
+        </div>
+        <div class="princ">
+            
+        </div>
+    </body>
+</html>
