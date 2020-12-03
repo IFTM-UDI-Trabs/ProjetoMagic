91e703f9f42c615ddee2a76b21de5e204f06328a
DouglasStuhler <DouglasStuhler@github.com>
2020-12-03 11:37:47 -0300

    DEU CERTO PEGO ESSA PORRA

--- cad_user.php
+++ cad_user.php
@@ -7,7 +7,7 @@
         <link rel="preconnect" href="https://fonts.gstatic.com">
         <link href="https://fonts.googleapis.com/css2?family=Open+Sans:ital,wght@0,300;0,400;0,600;0,700;0,800;1,300;1,400;1,600;1,700;1,800&display=swap" rel="stylesheet">
 
-        <link rel="stylesheet" type="text/css" href="index.css">
+        <link rel="stylesheet" type="text/css" href="cad_user.css">
     </head>
     <body>
         <div class="princ">
@@ -41,7 +41,7 @@
 
                 <div class="botoes">
                     <input type="submit" value="Confirme" class="button">
-                    <a href="index.php" class="button">Voltar</a>
+                    <a href="index.php" class="button dois">Voltar</a>
                 </div>
             </form>
 
