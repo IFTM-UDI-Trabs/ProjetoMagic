5552894068ed0304548846daeda67cceb0b7d7f6
DouglasStuhler <DouglasStuhler@github.com>
2020-12-10 16:03:19 -0300

    Acho que deu certo

--- pag_princi.php
+++ pag_princi.php
@@ -182,128 +182,149 @@
             ?>
             <div class="pag">
                 <div class="magias">
-            <?php
+                <?php
 
-            $conetca = conecta();
-
-            $texto_sql = @ "SELECT * FROM cad_user WHERE nome='$nome'";
-            $nome_fav = @ mysqli_query($conetca, $texto_sql);
-
-            while ($linha = mysqli_fetch_array($nome_fav)){
-                $nome_fav_result = $linha['magias'];
-            }
-
-            $contador = substr_count($nome_fav_result, ";");
-            $eba = $contador;
-            $letra = strlen($nome_fav_result);
-            // mensagem($contador);
-            $x = 0;
-
-            $nomes = array();
-
-            while($contador >= $x){
-                if ($eba == 0){
-                    $pontoVirgula = (strripos($nome_fav_result, ":") + 1);
-                    $nomes[$x] = substr($nome_fav_result, $pontoVirgula);
-                } else {
-                    $pontoVirgula = stripos($nome_fav_result, ";");
-                    $nomes[$x] = substr($nome_fav_result, 0, $pontoVirgula);
-                    $nome_fav_result = substr_replace($nome_fav_result, ":", $pontoVirgula, 1);
-                    $eba = substr_count($nome_fav_result, ";");
-                }
-                
-                // mensagem("nomes[$x]: ".$nomes[$x]);
-                $x += 1;
-                // mensagem("Contador while($x): ".$contador);
-            }
-
-            // if ($nome_fav == "" || $nome_fav == " "){} else {
-            //     $nome_fav_result = str_replace(";", " ", $nome_fav_result);
-            //     mensagem($nome_fav_result);
-            // }
-
-            $x = 0;
-            $y = count($nomes) - 1;
-
-
-            while ($x <= $y){
-                $nome_magia = $nomes[$x];
                 $conetca = conecta();
-                $texto_sql = @ "SELECT * FROM magia WHERE nome='$nome_magia'";
-                $resultado = @ mysqli_query($conetca, $texto_sql);
-                $selecao = mysqli_num_rows($resultado);
 
-                $q = 0;
-                $s = 0;
-                
-                if ($q == 0){
-                    echo "<div class='linha'>";
+                $texto_sql = @ "SELECT * FROM cad_user WHERE nome='$nome'";
+                $nome_fav = @ mysqli_query($conetca, $texto_sql);
+
+                while ($linha = mysqli_fetch_array($nome_fav)){
+                    $nome_fav_result = $linha['magias'];
                 }
-                while ($linha = mysqli_fetch_array($resultado)){
-                        $id = $linha['id'];
-                        $nome_magia = $linha['nome'];
-                        $escola_magia = $linha['escola'];
-                        $nivel_magia = $linha['nivel'];
-                        $conjurador_magia = $linha['conjurador'];
-                        $tempo_magia = $linha['tempoconj'];
-                        $alcance_magia = $linha['alcance'];
-                        $componentes_magia = $linha['componentes'];
-                        $duracao_magia = $linha['duracao'];
-                        $descricao_magia = $linha['descricao'];
-                        $descricaolvl_magia = $linha['descricaolvl'];
-                        $like_magia = $linha['curtidas'];
-                        // echo strstr($conjurador_magia, ',', false);
 
-                        $texto_sql = @ "SELECT * FROM cad_user WHERE nome='$nome'";
-                        $result = @ mysqli_query($conetca, $texto_sql);
+                $contador = substr_count($nome_fav_result, ";");
+                $eba = $contador;
+                $eba -= 1;
+                mensagem($eba);
+                $letra = strlen($nome_fav_result);
+                $x = 1;
 
-                        while ($linha = mysqli_fetch_array($result)){
-                            $magias = $linha['magias'];
+                $nomes = array();
+
+                $pontoVirgula = stripos($nome_fav_result, ";");
+                $nome_fav_result = substr_replace($nome_fav_result, "", $pontoVirgula, 1);
+
+                if (stripos($nome_fav_result, ";") == false){
+                    $nomes[0] = $nome_fav_result;
+                    mensagem($nomes[0]);
+                } else {
+                    while($contador >= $x){
+                        if ($eba == 0){
+                            $pontoVirgula = (strripos($nome_fav_result, ":") + 1);
+                            $nomes[$x] = substr($nome_fav_result, $pontoVirgula);
+                            mensagem($nomes["$x"]);
+                        } else {
+                            $pontoVirgula = stripos($nome_fav_result, ";");
+                            $nome_fav_result = substr_replace($nome_fav_result, ":", $pontoVirgula, 1);
+                            $nomes[$x] = substr($nome_fav_result, 0, $pontoVirgula);
+                            mensagem($nomes[$x]);
+                            $eba = substr_count($nome_fav_result, ";");
+                            mensagem($eba);
                         }
+                        
+                        $x += 1;
+                    }
+                }
 
-                        ?>
-                        <div class="box_total">
-                            <div class="box_inicio">
-                                <div class="title">
+                // while($contador >= $x){
+                //     if ($eba == 0){
+                //         $pontoVirgula = (strripos($nome_fav_result, ":") + 1);
+                //         $nomes[$x] = substr($nome_fav_result, $pontoVirgula);
+                //     } else {
+                //         $pontoVirgula = stripos($nome_fav_result, ";");
+                //         $nome_fav_result = substr_replace($nome_fav_result, "", $pontoVirgula, 1);
+                //         mensagem($nome_fav_result);
+                //         $pontoVirgula = stripos($nome_fav_result, ";");
+                //         $nomes[$x] = substr($nome_fav_result, 1, $pontoVirgula);
+                //         $eba = substr_count($nome_fav_result, ";");
+                //     }
+                    
+                //     $x += 1;
+                // }
+
+
+                $x = 0;
+                $y = count($nomes) - 1;
+
+                if (count($nomes) > 1){
+                    $x = 1;
+                    $y = count($nomes);
+                }
+
+
+                while ($x <= $y){
+                    $nome_magia = $nomes[$x];
+                    mensagem($nome_magia);
+                    $conetca = conecta();
+                    $texto_sql = @ "SELECT * FROM magia WHERE nome='$nome_magia'";
+                    $resultado = @ mysqli_query($conetca, $texto_sql);
+                    $selecao = mysqli_num_rows($resultado);
+
+                    $q = 0;
+                    $s = 0;
+                    
+                    if ($q == 0){
+                        echo "<div class='linha_full'>";
+                    }
+                    while ($linha = mysqli_fetch_array($resultado)){
+                            $id = $linha['id'];
+                            $nome_magia = $linha['nome'];
+                            $escola_magia = $linha['escola'];
+                            $nivel_magia = $linha['nivel'];
+                            $conjurador_magia = $linha['conjurador'];
+                            $tempo_magia = $linha['tempoconj'];
+                            $alcance_magia = $linha['alcance'];
+                            $componentes_magia = $linha['componentes'];
+                            $duracao_magia = $linha['duracao'];
+                            $descricao_magia = $linha['descricao'];
+                            $descricaolvl_magia = $linha['descricaolvl'];
+                            $like_magia = $linha['curtidas'];
+                            // echo strstr($conjurador_magia, ',', false);
+
+                            ?>
+                            <div class="box_total">
+                                <div class="box_inicio">
+                                    <div class="title">
+                                        <?php
+                                        echo "<p>$nome_magia</p>";
+                                        ?>
+                                    </div>
+                                    <div class="level">
+                                        <?php
+                                        echo "<p>$nivel_magia º nível de $escola_magia</p>"
+                                        ?>
+                                    </div>
+                                </div>
+                                <div class="info_basica">
                                     <?php
-                                    echo "<p>$nome_magia</p>";
+                                    echo "<p><span class='sub'>Conjugadores:</span> <span class='conj'>$conjurador_magia</span></p>";
+                                    echo "<p><span class='sub'>Alcance:</span> $alcance_magia</p>";
+                                    echo "<p><span class='sub'>Tempo de Conjuração:</span> $tempo_magia</p>";
+                                    echo "<p><span class='sub'>Componentes:</span> $componentes_magia</p>";
+                                    echo "<p><span class='sub'>Duração:</span> $duracao_magia</p>";
                                     ?>
                                 </div>
-                                <div class="level">
+                                <div class="desc">
                                     <?php
-                                    echo "<p>$nivel_magia º nível de $escola_magia</p>"
+                                    echo "<p>$descricao_magia</p>";
+                                    echo "<p><span class='sub'>Níveis Superiores:</span> $descricaolvl_magia</p>";
                                     ?>
                                 </div>
-                            </div>
-                            <div class="info_basica">
-                                <?php
-                                echo "<p><span class='sub'>Conjugadores:</span> <span class='conj'>$conjurador_magia</span></p>";
-                                echo "<p><span class='sub'>Alcance:</span> $alcance_magia</p>";
-                                echo "<p><span class='sub'>Tempo de Conjuração:</span> $tempo_magia</p>";
-                                echo "<p><span class='sub'>Componentes:</span> $componentes_magia</p>";
-                                echo "<p><span class='sub'>Duração:</span> $duracao_magia</p>";
-                                ?>
-                            </div>
-                            <div class="desc">
-                                <?php
-                                echo "<p>$descricao_magia</p>";
-                                echo "<p><span class='sub'>Níveis Superiores:</span> $descricaolvl_magia</p>";
-                                ?>
-                            </div>
-                            <div class="like">
-                                <?php
-                                if (stripos($magias, $nome_magia) == false){
-                                    echo "<form action='?op=3' id='form$id' method='POST'>";
-                                    echo "<button type='submit' name='id' value='$id'><img src='img/pocao_vazio.png' class='imgPocao'id='$id'onclick='animacao($id)'></button>";
-                                } else {
-                                    echo "<form action='?op=3' id='form$id' method='POST'>";
-                                    echo "<button type='submit' name='id' value='$id'><img src='img/pocao_like.png' class='imgPocao'id='$id'onclick='animacao($id)'></button>";
-                                }
-                                ?>
-                                </form>
+                                <div class="like">
+                                    <?php
+                                        echo "<form action='?op=3' id='form$id' method='POST'>";
+                                        echo "<button type='submit' name='id' value='$id'><img src='img/pocao_like.png' class='imgPocao'id='$id'onclick='animacao($id)'></button>";
+                                    ?>
+                                    </form>
+                                </div>
                             </div>
                         </div>
+                    <?php
+                    }
+                    ?>
                 <?php
+                $x += 1;
                 $q += 1;
                 $s += 1;
                 if ($q == 2){
@@ -316,12 +337,9 @@
                 }
                 }
                 ?>
-                    </div>
-                </div> <!-- Fechando DIV pag  -->
-                <?php
-                $x += 1;
-            }
-
+                </div>
+            </div> <!-- Fechando DIV pag  -->
+            <?php
         } else if ($op == 2){
             ?>
             
