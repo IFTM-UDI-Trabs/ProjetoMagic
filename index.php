88a0a58ad5666b178abd7d2515f9cd48f8ceed3c
DouglasStuhler <DouglasStuhler@github.com>
2020-12-11 22:02:55 -0300

    Acho q ficou legal...

--- index.php
+++ index.php
@@ -31,23 +31,26 @@
             </div>
         </div>
         <div id="rpg" class="azul">
-            <div class="texto">
-                <h1>O que é RPG?</h1>
-                <p class="txt">Role-Playing game, também conhecido como, RPG é um estilo de jogo em que os jogadores interpretam papéis de personagens, criando narrativas colaborativamente. O desenvolvimento do jogo depende puramente de um sistema de regras predeterminado, mas muitas vezes os jogadores preferem improvisar livremente.</p>
-                <p class="txt">Para esse site, o sistema de regras que utilizaremos como base é o tão aclamado “Dungeons & Dragons”, que se passa em um mundo de alta fantasia medieval onde existem criaturas épicas, heróis lendários, elfos, anões, gnomos, magos, guerreiros, monstros, e claro… Magia!</p>
+            <div class="prim_txt">
+                <div class="texto">
+                    <h1>O que é RPG?</h1>
+                    <p class="txt_azul">Role-Playing game, também conhecido como, RPG é um estilo de jogo em que os jogadores interpretam papéis de personagens, criando narrativas colaborativamente. O desenvolvimento do jogo depende puramente de um sistema de regras predeterminado, mas muitas vezes os jogadores preferem improvisar livremente.</p>
+                    <p class="txt">Para esse site, o sistema de regras que utilizaremos como base é o tão aclamado “Dungeons & Dragons”, que se passa em um mundo de alta fantasia medieval onde existem criaturas épicas, heróis lendários, elfos, anões, gnomos, magos, guerreiros, monstros, e claro… Magia!</p>
+                </div>
+                <img src="img/ded.png" class="ded">
+            </div>
+            <div class="seg_txt">
                 <p class="txt">O universo de Dungeons & Dragons é muito completo e cheio de histórias para contar, suas regras incluem classes e raças diferentes, proporcionando aventuras incríveis para os jogadores. Muitas vezes, para regrar esse sistema, são utilizados dados, de diferentes tamanhos e formas, que de certa maneira são a “marca” do RPG, o mais famoso deles é o D20, o dado de 20 lados.</p>
             </div>
-            <img src="img/ded.png">
         </div>
         <div id="objetivos" class="objetivos">
-            <img src="img/logo.png" class="img">
+            <img src="img/objetivo.png" class="obj">
             <div class="texto">
                 <h1>Objetivos</h1>
                 <p class="txt">Nossa intenção com este site é dar um “empurrãozinho” para aquelas pessoas que estão começando a se relacionar com o RPG, dando uma melhor introdução às magias, que é um sistema bem complexo no universo de Dungeons & Dragons, principalmente para aqueles que estão iniciando. Isso tudo, claro, com a ajuda de outras pessoas, que estarão interagindo com o site.</p>
             </div>
         </div>
         <div id="criadores" class="criadores">
-            <img src="img/logo.png" class="img">
             <div class="texto">
                 <h1>Criadores</h1>
                 <p class="txt">Novamente, nós, Douglas Stuhler Pinheiro e Murilo Silva Reis, somos estudantes do Instituto Federal do Triângulo Mineiro Campus Uberlândia, e fazemos o curso Técnico em Manutenção e Suporte em Informática. Tivemos a ideia de construir este site através de um projeto desenvolvido pelo professor Carlos Alberto Lopes. Também fazemos parte da comunidade de RPG, e por isso decidimos juntar os dois universos: O da programação com o da fantasia.</p>
